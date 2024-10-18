ef20b06f365125fab31d0750a2ea75416796871e
DChinni8 <diegochinni04@gmail.com>
2024-10-18 22:36:18 +0200

    Inserimento commenti descrittivi e spacing specifico

--- sketch.js
+++ sketch.js
@@ -1,35 +1,44 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  noLoop()
-  frameRate(1);
-}
-
-function draw() {
-  background("white");
-  fill("black");
-
-  let size = (15);
-  let spacing = random(0,20);
-  let XnColonne = windowWidth/(20+spacing); // Numero colonne
-  let YnRighe = windowHeight/(20+spacing); // Numero righe
-
-  // Calcolo della larghezza e dell'altezza totali della griglia
-  let gridWidth = XnColonne * (size + spacing) - spacing;
-  let gridHeight = YnRighe * (size + spacing) - spacing;
-
-  // Calcolo delle posizioni iniziali di x e y per centrare la griglia
-  let startX = (width - gridWidth) / 2;
-  let startY = (height - gridHeight) / 2;
-
-  for (let qx = 0; qx < XnColonne; qx++) {
-    for (let qy = 0; qy < YnRighe; qy++) {
-      push();
-      translate(startX + qx * (size + spacing) + size/2, startY + qy * (size + spacing) + size/2);
-      if (random(1) < 0.5) {
-        rotate(PI/4);
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+    // Disattivando questo noLoop si possono visualizzare le 4 opere cambiare randomicamente
+    noLoop();
+    frameRate(1);
+  }
+  
+  function draw() {
+    background("white"); // Imposta lo sfondo su bianco
+    fill("black"); // Imposta il colore di riempimento dei quadrati su nero
+  
+    // Misura lato dei quadrati
+    let size = 15;
+    // La spaziatura variabile tra i quadrati, una spaziatura per ciascuna delle 4 opere di Vera Molnar
+    let spacing = random([0, 5, 7.5, 15]);
+  
+    // Calcola il numero di colonne e righe in base alle dimensioni della finestra e alla spaziatura
+    let XnColonne = windowWidth / (18 + spacing);
+    let YnRighe = windowHeight / (18 + spacing);
+  
+    // Calcola la larghezza e l'altezza totali della griglia
+    let gridWidth = XnColonne * (size + spacing) - spacing;
+    let gridHeight = YnRighe * (size + spacing) - spacing;
+  
+    // Calcola le posizioni iniziali di x e y per centrare la griglia al centro della finestra
+    let startX = (width - gridWidth) / 2;
+    let startY = (height - gridHeight) / 2;
+  
+    // Questo ciclo for serve a disegnare la griglia di quadrati
+    for (let qx = 0; qx < XnColonne; qx++) {
+      for (let qy = 0; qy < YnRighe; qy++) {
+        push();
+        // Calcola le coordinate del centro dei quadrati
+        translate(startX + qx * (size + spacing) + size / 2, startY + qy * (size + spacing) + size / 2);
+        // Serve a ruotare i quadrati con una probabilità del 50%
+        if (random(1) < 0.5) {
+          rotate(PI / 4);
+        }
+        // Serve per disegnare il quadrato centrato nell'origine
+        rect(-size / 2, -size / 2, size, size);
+        pop(); 
       }
-      rect(-size/2, -size/2, size, size);
-      pop();
     }
   }
-}
